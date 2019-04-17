7dfdd0334b836a787339807fa328968c07fed2a3
1016184576 <miaozheng.0406@qq.com>
2019-04-17 14:31:41 +0800

    修改http方法

--- api/ajax.js
+++ api/ajax.js
@@ -1,5 +1,11 @@
 let ajax = 1;
 
-let $http = function(){
-  
+let $http = function(flag){
+  return new Promise((resolve,reject)=>{
+    if(flag){
+      resolve('请求成功');
+    }else{
+      resolve('请求失败');
+    }
+  })
 }
